422e1dd85c16c13e9ababefab3f091efe5e2db07
Gabriel Faria Zulianelli <gabrielzuli22@gmail.com>
2026-07-03 16:38:47 +0000

    Adicionado listar_tarefa_especifica

--- main.py
+++ main.py
@@ -26,6 +26,22 @@ def listat_tarefas():
 
     # Lista tarefas (somente id e titulo)
     if len(LISTA_TAREFAS) == 0:
-         return LISTA_TAREFAS
+        return LISTA_TAREFAS
     
     return [{"id": tarefa['id'], "titulo": tarefa['titulo']} for tarefa in LISTA_TAREFAS]
+
+@APP.get("/tarefas/{id}")
+def listar_tarefa_especifica(id: int):
+    global LISTA_TAREFAS
+    LISTA_TAREFAS.append(nova_tarefa(0, "nova tarefa", "descricao nova tarefa"))
+    mensagem_padrao = {"mensagem": "Não existe nenhuma tarefa"}
+
+    # Lista tarefas (somente id e titulo)
+    if len(LISTA_TAREFAS) == 0:
+        return mensagem_padrao    
+
+    # ID da tarefa é o índice da lista
+    if id >= 0 and id < len(LISTA_TAREFAS):
+        return LISTA_TAREFAS[id]
+    else:
+        return mensagem_padrao
