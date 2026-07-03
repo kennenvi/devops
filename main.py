852c7ff016355966b029d92ab9563adce693602f
Gabriel Faria Zulianelli <gabrielzuli22@gmail.com>
2026-07-03 16:28:46 +0000

    Adicionado listar_tarefas

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
+from datetime import datetime
+
 from fastapi import FastAPI
 
 
+LISTA_TAREFAS = []
 APP = FastAPI()
+
+def nova_tarefa(id: int, titulo: str, descricao: str):
+    """Função auxiliar para criar uma tarefa usando dicionário (`dict`)"""
+    return {
+        "id": id,
+        "titulo": titulo,
+        "descricao": descricao,
+        "concluido": False,
+        "criado_em": datetime.now()
+    }
 
 @APP.get("/")
 def index():
     return "Olá, DevOps!"
+
+@APP.get("/tarefas")
+def listat_tarefas():
+    global LISTA_TAREFAS
+
+    # Lista tarefas (somente id e titulo)
+    if len(LISTA_TAREFAS) == 0:
+         return LISTA_TAREFAS
+    
+    return [{"id": tarefa['id'], "titulo": tarefa['titulo']} for tarefa in LISTA_TAREFAS]
